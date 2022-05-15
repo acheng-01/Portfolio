c50555af533f0c846a2113b86785c74574cd5f49
Andrew Cheng <password>
2022-05-15 05:05:41 -0700

    changed landing wording

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -6,7 +6,7 @@ function Landing() {
         <div className="landing-container" id="landing">
             <div className="inner-landing-container">
                 <h1>ANDREW CHENG</h1>
-                <h2>Fullstack Developer and Software Engineer.</h2>
+                <h2>Fullstack Software Engineer.</h2>
             </div>
         </div>
     )
