f0d9fb58e4a50dbfc06e16b85550165b15a29f20
Andrew Cheng <password>
2022-05-15 05:29:08 -0700

    fixed typing animation

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -4,8 +4,10 @@ import './Landing.css';
 function Landing() {
     return (
         <div className="landing-container" id="landing">
-            <div className="inner-landing-container">
+            <div className="upper-landing-container">
                 <h1>ANDREW CHENG</h1>
+            </div>
+            <div className="lower-landing-container">
                 <h2>Fullstack Software Engineer.</h2>
             </div>
         </div>
